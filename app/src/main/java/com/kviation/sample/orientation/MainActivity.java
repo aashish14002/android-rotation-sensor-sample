972afe06813539d19fc61a1a39686128f2e4c50c
aashish14002 <aashu347@gmail.com>
2017-09-15 13:53:53 +0530

    change mainActivity- add button for starting and stopping visualization write sensor reading to file

--- app/src/main/java/com/kviation/sample/orientation/MainActivity.java
+++ app/src/main/java/com/kviation/sample/orientation/MainActivity.java
@@ -2,12 +2,14 @@ package com.kviation.sample.orientation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements Orientation.Listener {
 
   private Orientation mOrientation;
   private AttitudeIndicator mAttitudeIndicator;
-
+  private Button mButton;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -15,19 +17,40 @@ public class MainActivity extends AppCompatActivity implements Orientation.Liste
 
     mOrientation = new Orientation(this);
     mAttitudeIndicator = (AttitudeIndicator) findViewById(R.id.attitude_indicator);
+    mButton = (Button) findViewById(R.id.button);
+    mButton.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            if(mButton.getText()=="Stop"){
+                stop();
+            }else {
+                start();
+            }
+
+        }
+    });
   }
 
-  @Override
-  protected void onStart() {
-    super.onStart();
-    mOrientation.startListening(this);
-  }
+    protected void start(){
+        mOrientation.startListening(this);
+        mButton.setText("Stop");
+    }
 
-  @Override
-  protected void onStop() {
-    super.onStop();
-    mOrientation.stopListening();
-  }
+    protected void stop(){
+        mOrientation.stopListening();
+        mButton.setText("Start");
+    }
+
+//  @Override
+//  protected void onStart() {
+//    super.onStart();
+//    mOrientation.startListening(this);
+//  }
+//
+//  @Override
+//  protected void onStop() {
+//    super.onStop();
+//    mOrientation.stopListening();
+//  }
 
   @Override
   public void onOrientationChanged(float pitch, float roll) {
--- app/src/main/java/com/kviation/sample/orientation/Orientation.java
+++ app/src/main/java/com/kviation/sample/orientation/Orientation.java
@@ -2,13 +2,22 @@
 package com.kviation.sample.orientation;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.Surface;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Orientation implements SensorEventListener {
 
@@ -26,6 +35,7 @@ public class Orientation implements SensorEventListener {
   private final Sensor mAccelerometer;
 
   private final Sensor mGyroscope;
+  private Activity mactivity;
 
 
   private int mLastAccuracy;
@@ -34,7 +44,7 @@ public class Orientation implements SensorEventListener {
   public Orientation(Activity activity) {
     mWindowManager = activity.getWindow().getWindowManager();
     mSensorManager = (SensorManager) activity.getSystemService(Activity.SENSOR_SERVICE);
-
+    mactivity = activity;
     // Can be null if the sensor hardware is not available
     mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
@@ -79,10 +89,28 @@ public class Orientation implements SensorEventListener {
       return;
     }
     if (event.sensor == mAccelerometer || event.sensor == mGyroscope) {
+      String readings = event.sensor.getName()+","+event.values[0]+","+event.values[1]+","+event.values[2];
+      writeSensorData(readings);
       updateOrientation(event.sensor.getName(), event.values);
     }
   }
 
+  public void writeSensorData(String readings)
+  {
+    String fileName="sensor-data.txt";
+    File file= new File(mactivity.getFilesDir(), fileName);
+    String f = String.valueOf(file);
+    try {
+      FileOutputStream fout=new FileOutputStream(f,true);
+      fout.write(readings.getBytes());
+      fout.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
   @SuppressWarnings("SuspiciousNameCombination")
   private void updateOrientation(String sensor, float[] rotationVector) {
     float[] rotationMatrix = new float[9];
